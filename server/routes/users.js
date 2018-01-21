123f823598325ef887e2103148b5a9e07b78e791
Brendan Pete <brendan.m.pete@gmail.com>
2018-01-21 17:20:26 -0500

    clean up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,5 @@
 const express = require('express')
-// const assert = require('assert')
 const router = express.Router()
-// const MongoClient = require('mongodb').MongoClient
-const mongoose = require('mongoose')
-const keys = require('../keys')
 
 router.get('/', function(req, res, next) {
   res.send({
